f504f32598501edd5354cb2259fe33217a90ea72
abidhossain2 <tanvirabid229@gmail.com>
2022-05-22 02:04:00 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qlpqx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('Database connected');
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Listening to  ${port}`)
+})
