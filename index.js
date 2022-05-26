22bb314b4ad5ed94d759cc3f24f6c1ee3319fe83
abidhossain2 <tanvirabid229@gmail.com>
2022-05-26 12:10:25 +0600

    order delete api create

--- index.js
+++ index.js
@@ -123,6 +123,12 @@ async function run() {
       const result = await orderCollection.find(query).toArray()
       res.send(result)
     })
+    app.delete('/allorders/:id', async(req, res)=> {
+      const id = req.params.id
+      const query ={_id:ObjectId(id)}
+      const result = await orderCollection.deleteOne(query)
+      res.send(result)
+    })
 
     app.get('/user/:email', async (req, res) => {
       const email = req.params.email;
